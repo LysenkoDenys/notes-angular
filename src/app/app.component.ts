69f8a1b80a7c803171b6bdc782fbf856fbef6c92
Lysenko <lysenkoden@gmail.com>
2024-09-03 22:44:02 +0200

    initial angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, MenuComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'notes';
+  isRed = false;
+  change() {
+    this.isRed = !this.isRed;
+  }
+
+  products = ['apple', 'orange', 'banana'];
+
+  isLoggedIn = false;
+
+  change2() {
+    this.isLoggedIn = !this.isLoggedIn;
+    console.log(this.isLoggedIn);
+  }
 }
